e9257bcec25285529c18fc6c7d503679c8ccc510
Midhu <midhu.jh@ayatacommerce.com>
2024-06-17 12:14:00 +0530

    Display Hello World

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,11 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use('/users', userRoutes);
 
+// Root route to display Hello, World!
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
